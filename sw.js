c5a8af8d839bbad6e4c9ea22f3fad186cbf68e19
thanuka2003 <thanuka200310@gmail.com>
2026-07-04 06:16:36 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = "z-pinger-v1";
+const ASSETS = ["./","./index.html","./manifest.json","./icon128.png","./icon192.png","./icon512.png"];
+
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).catch(() => {}));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  if (req.url.includes("favicon.ico?_=")) return;
+  if (req.method !== "GET") return;
+  e.respondWith(
+    caches.match(req).then((cached) =>
+      cached || fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {});
+        return res;
+      }).catch(() => caches.match("./index.html"))
+    )
+  );
+});
